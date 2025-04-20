ab04166a21b60fbc24e65782a210e2e1ed1c1182
SlimeNull <slimenull@outlook.com>
2025-04-20 20:07:20 +0800

    Add project files.

---
+++ WhyNotMemoryMap/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace WhyNotMemoryMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class StreamExtensions
+    {
+        public static unsafe T ReadStruct<T>(this Stream stream)
+            where T : unmanaged
+        {
+            var size = sizeof(T);
+            var buffer = new byte[size];
+
+            var bytesRead = 0;
+
+            do
+            {
+                stream.Read(buffer, bytesRead, size - bytesRead);
+            }
+            while (bytesRead < size);
+
+            fixed (byte* ptr = buffer)
+            {
+                return *(T*)ptr;
+            }
+        }
+
+        public static unsafe void WriteStruct<T>(this Stream stream, T value)
+            where T : unmanaged
+        {
+            var size = sizeof(T);
+            var buffer = new byte[size];
+
+            byte* dataPtr = (byte*)&value;
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = dataPtr[i];
+            }
+
+            stream.Write(buffer);
+        }
+    }
+}
