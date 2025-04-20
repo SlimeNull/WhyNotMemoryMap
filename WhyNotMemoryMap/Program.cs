40b677758fba986f1de4273247c75900e332cef2
SlimeNull <slimenull@outlook.com>
2025-04-20 22:06:46 +0800

    feat: 添加高性能实现

--- WhyNotMemoryMap/Program.cs
+++ WhyNotMemoryMap/Program.cs
@@ -14,9 +14,20 @@
             where T : unmanaged
         {
             var size = sizeof(T);
-            var buffer = new byte[size];
-
             var bytesRead = 0;
+
+#if NETCOREAPP2_1_OR_GREATER
+            T result = default(T);
+            do
+            {
+                Span<byte> buffer = new Span<byte>(((byte*)&result) + bytesRead, size - bytesRead);
+                stream.Read(buffer);
+            }
+            while (bytesRead < size);
+
+            return result;
+#else
+            var buffer = new byte[size];
 
             do
             {
@@ -28,12 +39,17 @@
             {
                 return *(T*)ptr;
             }
+#endif
         }
 
         public static unsafe void WriteStruct<T>(this Stream stream, T value)
             where T : unmanaged
         {
             var size = sizeof(T);
+#if NETCOREAPP2_1_OR_GREATER
+            var buffer = new Span<byte>(&value, size);
+            stream.Write(buffer);
+#else
             var buffer = new byte[size];
 
             byte* dataPtr = (byte*)&value;
@@ -43,6 +59,7 @@
             }
 
             stream.Write(buffer);
+#endif
         }
     }
 }
